3a1c51ca42ada63df18777bb62eae3f616fc407b
royalkingzeher <raghavmittal26113@gmail.com>
2023-09-19 09:03:50 +0530

    first commit

---
+++ devsecops.b3.cicd/src/main/java/devsecops/b3/cicd/MyClass.java
@@ -0,0 +1,14 @@
+package devsecops.b3.cicd;
+
+public class MyClass {
+	public void greeting()
+	{
+		System.out.println("Hello GitHub Actions");
+	}
+	public static void main(String[] args) {
+		MyClass ob = new MyClass();
+		ob.greeting();
+
+	}
+
+}
